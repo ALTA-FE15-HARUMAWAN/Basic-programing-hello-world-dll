078098f820919577ae2fcdec42f3d04df9820e16
HARUMAWAN <mharumawan@gmail.com>
2023-07-06 14:15:39 +0700

    upload tugas_typenscript

---
+++ typescript/tugas_typenscript.ts
@@ -0,0 +1,19 @@
+//Problem 1 : program hello nama 
+let HARUM1  = "hello harum"
+let HARUM2  = "!!!SAYA JAVASCRIPT, BAHASA PEMROGRAMAN YANG MENYENANGKAN"
+console.log(HARUM1 + " " + HARUM2)
+
+//Problem 2 : Luas Segitiga
+let a   = 24 
+let T  = 20 
+let s  = 1/2
+let mul  = a * T * s
+console.log("luas segitiga " + "=" + " " + mul + " " + " cm ")
+
+// Problem 3 : Luas permukaan tabung
+let r = 7
+let t = 20
+let phi = 22/7
+let Q = r + t
+let LP = 2 * phi * r * Q
+console.log("Luas Permukaan Tabung" + " " + "=" + " " + LP + " " + "cm" )
